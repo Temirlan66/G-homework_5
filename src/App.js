0f7d1ae62077654602167fd378134f7a2a1ad7ee
Temirlan <admin@Temirlans-MacBook-Pro.local>
2023-05-30 23:31:37 +0600

    hm5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  useEffect(() => {
+    const raw = localStorage.getItem("todos") || [];
+    setTodos(JSON.parse(raw));
+  }, []);
+
+  const addItem = () => {
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: title,
+    };
+
+    setTodos((prevState) => [...prevState, item]);
+    setTitle("");
+    console.log(todos);
+  };
+
+  const deleteItem = (id) => {
+    const array = todos.filter((item) => item.id !== id);
+    setTodos(array);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <div className="container">
+        <input value={title} onChange={(e) => setTitle(e.target.value)} />
+        <button onClick={addItem}>ADD</button>
+      </div>
+
+      <ul className="ul-list">
+        {todos.map((item) => {
+          return (
+            <li key={item.id}>
+              {item.value}
+              <button onClick={() => deleteItem(item.id)}>delete</button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
